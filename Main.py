f633da2860ad7d84aaf0ef92cb95709bc060a7a0
Atharv Kothari <87928064+Atharv2122@users.noreply.github.com>
2026-05-13 07:35:03 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import datetime as dt
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv('2025_Yellow_Taxi_Trip_Data.csv')
+
+df.head()
